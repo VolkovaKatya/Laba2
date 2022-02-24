49353f547d76322bb597d9fc0103826c63577d2a
VolkovaKatya <katya.volkova2002@yandex.ru>
2022-02-25 00:27:16 +0200

    number 14

--- laba2.php
+++ laba2.php
@@ -45,3 +45,77 @@ $answer -= 2;
 $answer /= 2;
 $answer -= $my_num;
 echo $answer;
+
+//number 14
+$a = 10;
+$b = 3;
+echo "<br>" . ($a % $b);
+if ($a % $b == 0)
+    echo "<br>Делится<br>";
+else echo "<br>Делится с остатком<br>" . ($a % $b);
+
+echo $st = pow(2,10) . "<br>";
+echo sqrt(245) . "<br>";
+$mas1 = array(4,2,5,19,13,0,10);
+$sum = 0;
+foreach($mas1 as $num)
+    $sum += $num**2;
+echo sqrt($sum). "<br>";
+
+echo round(sqrt(379),0) . " ";
+echo round(sqrt(379),1) . " ";
+echo round(sqrt(379),2) . "<br>";
+$mas2 = array(
+    "floor" => floor(sqrt(587)),
+    "ceil" => ceil(sqrt(587)));
+
+$mas3=array(4,-2,5,10,-130,0,10);
+$min = $mas3[0];
+$max = $mas3[0];
+foreach($mas3 as $numb)
+    echo"$numb ";
+if ($min > $numb) $min = $numb;
+if ($max < $numb) $max = $numb;
+echo"<br>max - $max , min - $min";
+
+echo "<br>" . rand(1,100) . "<br>";
+$mas4 = array();
+for($i = 0; $i < 10; $i++) {
+    array_push($mas4, rand(1, 100));
+}
+for($i = 0; $i < 10; $i++) {
+    echo "$mas4[$i] ";
+}
+
+for($i = 0; $i < 10; $i++) {
+    $a = rand(-100,100);
+    $b = rand(-100,100);
+    echo "<br>a-b=$a-$b=|" . ($a-$b) . "|=" . abs($a-$b);
+}
+
+$mas5 = array();
+for($i = 0; $i<10; $i++) {
+    array_push($mas5, rand(-100, 100));
+}
+foreach ($mas5 as &$number) {
+    echo $number . " ";
+    if($number < 0)
+        $number = abs($number);
+}
+
+$numeric = rand(1,100);
+echo "<br>Number: $numeric<br>";
+$dividers = array();
+for($i = 1; $i <= $numeric; $i++) {
+    if($numeric % $i == 0) {
+        array_push($dividers, $i);
+        echo $i . " ";
+    }
+}
+$mas6 = array(1,2,3,4,5,6,7,8,9,10);
+$sum = 0;
+$count = 0;
+while ($sum < 10) {
+    $sum += $mas6[$count];
+    $count ++;
+}
