9f14cf6dcfdcb5a38daf17d4eb4bc3d0916ed327
VolkovaKatya <katya.volkova2002@yandex.ru>
2022-02-24 22:44:22 +0200

    number 1

--- laba2.php
+++ laba2.php
@@ -1,1 +1,14 @@
 <?php
+
+//number 1
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name="15 chicken wings";
+
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= " Hello World!";
+
+//// Don't change the line below
+
+echo "<br>Your order is:$very_bad_unclear_name.";
